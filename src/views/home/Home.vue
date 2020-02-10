2680fae74058acca2654e2e0ebcf2a7022afeca3
zengyuan <canoncqzy@163.com>
2020-02-11 00:37:56 +0800

    axios config

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,25 +1,45 @@
 <!-- Home -->
 <template>
-  <div>
-    <h2>Home</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar"
+import {getHomeMultidata} from "network/home"
+
 export default {
   name: 'Home',
   data () {
     return {
+      result: null
     };
   },
 
-  components: {},
+  components: {
+    NavBar
+  },
 
   computed: {},
 
-  methods: {}
+  methods: {},
+
+  created() {
+    
+    getHomeMultidata().then(res => {
+      console.log(res.data)
+      this.result = res.data
+    })
+  }
 }
 
 </script>
-<style lang='scss' scoped>
+<style scoped>
+.home-nav {
+  background: var(--color-tint);
+  color: white;
+}
 </style>
